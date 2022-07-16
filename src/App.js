cef86054b2346e52ef2d3e2932ac6c7cd5a1482f
sekretnaya <104978804+sekretnaya@users.noreply.github.com>
2022-07-16 14:26:14 +0500

    madinani shopping sayti
    boshlangich

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Homepage from './pages/js/Homepage.js'
+class App extends Component {
+    render() {
+        return(
+            <div>
+            <Homepage/>
+            </div>
+
+    );
+    }
+}
+
+export default App;
