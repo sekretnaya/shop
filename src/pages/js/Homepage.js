d9b800c567ef2657d39f6da8a7a68d23f706d726
sekretnaya <104978804+sekretnaya@users.noreply.github.com>
2022-07-16 14:31:07 +0500

    shhss
    kajsj

--- src/pages/js/Homepage.js
+++ src/pages/js/Homepage.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 import '../../App.css'
 import { FaMapMarkerAlt } from 'react-icons/fa'
 import { BsBarChartSteps } from 'react-icons/bs'
-import img from './logo.png'
+import img from "../img/logo.png"
 import { FiSearch } from 'react-icons/fi'
 import { TiShoppingCart } from 'react-icons/ti'
 export default class Homepage extends Component {
   render() {
-    return (
-      <div>
-      <div className="navbar1">
-    <div className="div12">
-  <div className="nav1">О нас</div>
+    return(<div>
+        <div className="navbar1">
+          <div className="div12">
+           <div className="nav1">О нас</div>
     <div className="nav2"><FaMapMarkerAlt/> Ташкент</div></div>
     <div className="nav3">
         <h3>+99899 000 00 00</h3>
@@ -19,7 +18,7 @@ export default class Homepage extends Component {
     </div>
 </div>
 <div className="navbar2">
-<img src={img} />
+<img src={img}  alt=" " />
  <div className="madi"><BsBarChartSteps/><span>Каталог</span></div>
 <div className="m444"><input placeholder="Что желаете найти?"className="tentak"type="text"/>< FiSearch className = "fisearch"/></div> 
 <div className="ggg"><TiShoppingCart/><sup>1</sup>Корзина</div></div>
